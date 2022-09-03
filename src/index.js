bc6be6616318edad0632bb6941b2b191bb21c7c0
asbonato <asbonato@gmail.com>
2022-09-03 10:58:03 -0300

    Apostila 4 finalizada

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 import Pedido from './Pedido'
+import Cartao from './Cartao'
+import Feedback from './Feedback'
 
 const App = () => {
+    const textoOK = "Já chegou"
+    const textoNOK = "Ainda não chegou"
+    const funcaoOK = () => alert("Agradecemos a confirmação!")
+    const funcaoNOK = () => alert("Verificaremos o ocorrido!")
+    const componenteFeedback = <Feedback textoOK={textoOK} funcaoOK={funcaoOK}
+        textoNOK={textoNOK} funcaoNOK={funcaoNOK}/>;
+
     return <div className='container border rounded mt-2'>
 
         {/**linha para o título */}
@@ -16,8 +25,11 @@ const App = () => {
         <div className='row'>
             {/**controle de colunas para responsividade */}
             <div className='col-sm-8 col-md-6 m-2'>
-                <Pedido data="22/04/2021" icone="fas fa-hdd fa-2x"
-                    titulo="SSD" descricao="SSD Kingston A400 - Sata"/>
+                <Cartao cabecalho="22/04/2021">
+                    <Pedido icone="fas fa-hdd fa-2x"
+                        titulo="SSD" descricao="SSD Kingston A400 - Sata"/>
+                    {componenteFeedback}
+                </Cartao> 
             </div>
         </div>
 
@@ -25,8 +37,11 @@ const App = () => {
         <div className='row'>
             {/**controle de colunas para responsividade */}
             <div className='col-sm-8 col-md-6 m-2'>
-            <Pedido data="20/04/2021" icone="fas fa-book fa-2x"
-                    titulo="Livro" descricao="Concrete Mathematics - Donald Knuth"/>
+                <Cartao cabecalho="20/04/2021">
+                    <Pedido icone="fas fa-book fa-2x"
+                        titulo="Livro" descricao="Concrete Mathematics - Donald Knuth"/>
+                    {componenteFeedback}
+                </Cartao> 
             </div>
         </div>
         
@@ -34,8 +49,11 @@ const App = () => {
         <div className='row'>
             {/**controle de colunas para responsividade */}
             <div className='col-sm-8 col-md-6 m-2'>
-            <Pedido data="21/01/2021" icone="fas fa-laptop fa-2x"
-                    titulo="Notebook" descricao="Notebook Dell - 8Gb - i5"/>
+                <Cartao cabecalho="21/01/2021" >
+                    <Pedido icone="fas fa-laptop fa-2x"
+                        titulo="Notebook" descricao="Notebook Dell - 8Gb - i5"/>
+                    {componenteFeedback}
+                </Cartao>
             </div>
         </div>
 
